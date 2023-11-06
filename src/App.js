368130984bac3724fd86aa02ec0dd8d1493b3d2e
Sachin_Cherey <64123014+sachinCherey@users.noreply.github.com>
2023-11-06 07:18:21 +0000

    done

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import WeatherApp from './WeatherApp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <WeatherApp/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/WeatherApp.js
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+const WeatherApp = () => {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleSearch = async () => {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=840de593b7028de6e424162454790fe5&units=metric`
+    );
+    const data = await response.json();
+    setWeatherData(data);
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: "75px",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "white",
+        borderRadius: "10px",
+        height: "100vh",
+        width: "50%",
+        margin: "0px auto"
+      }}
+    >
+      <div>
+        <input
+          type="text"
+          value={city}
+          onChange={handleInputChange}
+          placeholder="Enter city"
+        />
+        <button onClick={handleSearch}>Search</button>
+      </div>
+      {weatherData ? (
+        <div>
+          <h2>{weatherData.name}</h2>
+          <p>Temperature: {weatherData.main.temp}°C</p>
+          <p>
+            Weather: {weatherData.weather[0].main} - {weatherData.weather[0].description}
+          </p>
+        </div>
+      ) : (
+        <p>Your response should come here</p>
+      )}
+    </div>
+  );
+};
+
+export default WeatherApp;
